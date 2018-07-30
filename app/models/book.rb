ed98d9a26d7b911608d9b118edf8234fd4f3880b
RitheshBRao <ritheshb1@gmail.com>
2018-07-30 22:41:50 +0530

    BookReview app part-3 is done

---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ApplicationRecord
+	belongs_to :user
+	belongs_to :category
+
+	has_attached_file :book_img
+    validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
+end
