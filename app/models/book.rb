a00d2950111aeac7220aa2b8a385e4b81c6357ed
RitheshBRao <ritheshb1@gmail.com>
2018-08-02 23:19:58 +0530

    Added Review Controller, Review new form.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
-	has_many :review
+	has_many :reviews
 
 	has_attached_file :book_img
     validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
